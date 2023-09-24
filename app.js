6d0a186d7a0526bd01f1b0e0cf691ee8ad47a48d
anis10023 <m.anis.rahman@outlook.com>
2023-09-24 17:55:44 +0100

    Improved logic further

--- app.js
+++ app.js
@@ -2,16 +2,16 @@
 //Module pattern
 const Gameboard = (() => {
   let boxes = [
-    ["O", "O", "O"],
-    ["X", "X", "X"],
-    ["O", "O", "O"],
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
   ];
-  const add = (XO, row, column) => boxes[row].splice(column, 0, XO);
+  const add = (XO, row, column) => (boxes[row][column] = `${XO}`);
   const reset = () =>
     (boxes = [
-      [, ,],
-      [, ,],
-      [, ,],
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
     ]);
 
   return { boxes, add, reset };
@@ -64,31 +64,110 @@ function Player(name, number, type) {
   return { getName, getNumber, getType };
 }
 
+const player1 = Player("Player 1", 1, "X");
+const player2 = Player("Player 2", 2, "O");
+
 //&&User interface
 //Grab existing DOM elements
 const boxes = document.querySelectorAll(".box");
 const player1Score = document.querySelector(".player1Score");
 const player2Score = document.querySelector(".player2Score");
 
+function convert1D_array(index) {
+  //~Mapping 1D array to 2D array
+
+  //Vertical index: array1d[i] mod vertical_size
+  const column = index % 3;
+
+  //Horizontal index: Math.floor(i / horizontal_size)
+  const row = Math.floor(index / 3);
+
+  return { column, row };
+}
+
+function turnSwapping(index) {
+  const column = convert1D_array(index).column;
+  const row = convert1D_array(index).row;
+
+  //Prevent playing in spots already taken
+  if (
+    Gameboard.boxes[row][column] === "X" ||
+    Gameboard.boxes[row][column] === "O"
+  ) {
+    return;
+  }
+
+  // Swap turns
+  if (gameState.player1_turn) {
+    Gameboard.add("X", row, column);
+    gameState.player1_turn = false;
+    gameState.player2_turn = true;
+  } else if (gameState.player2_turn) {
+    Gameboard.add("O", row, column);
+    gameState.player2_turn = false;
+    gameState.player1_turn = true;
+  }
+}
+
+function isBoardFull() {
+  let spotsFilled = 0;
+  for (i = 0; i < Gameboard.boxes.length; i++) {
+    for (j = 0; j < Gameboard.boxes[i].length; j++) {
+      if (Gameboard.boxes[i][j] === "X" || Gameboard.boxes[i][j] === "O") {
+        spotsFilled++;
+        console.log(spotsFilled);
+      }
+    }
+  }
+  //   if (spotsFilled === 9) {
+  //     Gameboard.reset;
+  //     gameState.endGame = true;
+  //     boxes.forEach((box) => {
+  //       box.textContent = "";
+  //     });
+  //   }
+}
+
+function isWon() {
+  if (
+    (Gameboard.boxes[0][0] === Gameboard.boxes[0][0]) ===
+    Gameboard.boxes[0][0]
+  ) {
+  }
+}
+
+function gameRules() {
+  isBoardFull();
+}
+
 function displayAlgorithm() {
-  let counter = 0;
+  let index1D = 0;
   for (i = 0; i < Gameboard.boxes.length; i++) {
     for (j = 0; j < Gameboard.boxes[i].length; j++) {
       if (Gameboard.boxes[i][j] === "") {
-        counter++;
+        index1D++;
         continue;
       }
       if (Gameboard.boxes[i][j] !== "") {
-        boxes[counter].textContent = Gameboard.boxes[i][j];
+        boxes[index1D].textContent = Gameboard.boxes[i][j];
       }
-      counter++;
+      index1D++;
     }
   }
 }
 
-function render() {
+function render(index) {
+  turnSwapping(index);
   displayAlgorithm();
+  gameRules();
 }
-render();
 
-function getPlayerDetails() {}
+//Event listener
+boxes.forEach((box) => {
+  box.addEventListener("click", (e) => {
+    const index = e.target.getAttribute("data-id");
+    render(index);
+  });
+});
+
+// function getPlayerDetails() {}
